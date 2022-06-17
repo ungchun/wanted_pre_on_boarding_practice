a4cb26f645a3850cbd5762494e3cb4b765bbdd5d
ungchun <leedool3003@naver.com>
2022-06-18 00:55:41 +0900

    Initial Commit

---
+++ wanted_pre_on_boarding_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_on_boarding_practice
+//
+//  Created by 김성훈 on 2022/06/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
